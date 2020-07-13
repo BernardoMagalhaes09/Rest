d17ddb2646a75db6003be60d4de688458f922952
bernardombsouza <bernardo@arbyte.com.br>
2020-07-13 19:19:16 -0300

    CRUD PRONTO

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,23 +1,23 @@
 const {Router} = require('express')
 const router = new Router();
 
-const routName = '/orders'
+const routeName = '/orders'
 //Lista todos os pedidos
-router.get(routName, (req, res) => {
+router.get(routeName, (req, res) => {
     res.json([{
         message: 'Vai retornar todos os pedidos',
     }])
 });
 
 //Retorna um pedido expecifico
-router.get(`${routName}/:id`, (req, res) => {
+router.get(`${routeName}/:id`, (req, res) => {
         res.json([{message: 'Vai retornar os dados de um pedidos com um id', 
         id: req.params.id,
     }])
 });
 
 // Cria um pedido novo
-router.post(routName, (req, res) => {
+router.post(routeName, (req, res) => {
     const pedido = {
         idProduto: req.body.idProduto,
         quantidade: req.body.quantidade
@@ -30,7 +30,7 @@ router.post(routName, (req, res) => {
 });
 
 //Edita os dados de um pedido
-router.patch(`${routName}/:id`, (req, res) => {
+router.patch(`${routeName}/:id`, (req, res) => {
     res.json({
         message: 'Vai editar od dados de um pedido baseado em um id',
         id: req.params.id,
@@ -38,7 +38,7 @@ router.patch(`${routName}/:id`, (req, res) => {
 })
 
 //Deleta um pedido baseado no id
-router.delete(`${routName}/:id`, (req, res) => {
+router.delete(`${routeName}/:id`, (req, res) => {
     res.status(204).end()
 })
 
